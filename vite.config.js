3158ef5e41626fea371ea8f389bd26cbaceaeca8
enisabazi <enisabazi415work@gmail.com>
2025-05-24 20:49:59 +0200

    woip/1

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/enis-dev-portfolio/', // Correct base for GitHub Pages deployment
+  base: '/',
   plugins: [react()],
 })
+
 
